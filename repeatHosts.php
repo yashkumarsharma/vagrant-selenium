68e531c9d3edab7fa00a1463072b65a4d4942d7b
Slayer911 <slayer911@inbox.ru>
2017-07-17 10:01:18 +0300

    Update repeatHosts.php

--- repeatHosts.php
+++ repeatHosts.php
@@ -5,6 +5,6 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     $hostFilePath = '\etc\hosts';
 }
 $hostData = file_get_contents($hostFilePath);
-$hostData = preg_replace('/127.0.0.1/', '10.0.2.2', $hostData);
+$hostData = preg_replace('/127\.0\.0\.1/', '10.0.2.2', $hostData);
 $hostData = preg_replace('/10\.0\.2\.2\s*localhost/', null, $hostData);
 file_put_contents('hosts', $hostData);
